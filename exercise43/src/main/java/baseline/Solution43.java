7a6f3231d8cb3f2f03272df29fe5b22baef58e00
Rylan S <rylan.simpson.02@gmail.com>
2021-10-14 15:25:08 -0400

    Completed exercise 43

--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -1,7 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Rylan Simpson
+ */
+
 package baseline;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 class HTMLGenerator {
@@ -9,66 +16,83 @@ class HTMLGenerator {
     String[] siteInfo = {"", ""};
     boolean[] folders = {false, false};
 
-    private boolean checkForYes (String input) {
-        //If input is y or Y, return true
-        if () {
+    static final String START = "./data/website/";
 
-        }
-        //Else return/keep false
-        else {
-
-        }
+    public HTMLGenerator(String webName, String author, String javaChoice, String cssChoice) {
+        //Assign strings to website data
+        siteInfo[0] = webName;
+        siteInfo[1] = author;
+        //Check bool values for if user wants to create js/css directories
+        folders[0] = checkForYes(javaChoice);
+        folders[1] = checkForYes(cssChoice);
     }
 
-    //This method sets the values for the instance variables
-    void promptSiteInfo () {
-        Scanner input = new Scanner(System.in);
-        //Prompt user for site name
-        System.out.printf("%n");
-        //Save user input
-        siteInfo[0];
-        //Prompt user for site author
-        System.out.printf("%n");
-        //Save user input
-        siteInfo[1];
-        //Prompt user for if they want java folder
-        System.out.printf("%n");
-        //Save user input
-        folders[0];
-        //Prompt user for css file
-        System.out.printf("%n");
-        //Save user input
-        folders[1];
+    private boolean checkForYes (String input) {
+        //If input is y or Y, return true
+        return input.equals("y") || input.equals("Y");
     }
 
     void generateSiteData() {
         //Make directory for website data
-        new File ();
-        //Print creation verification
+        File website = new File (START+siteInfo[0]);
+        if(website.mkdirs()) {
+            //Print creation verification
+            if (website.isDirectory()) {
+                //Print confirmation message
+                System.out.printf("Created %s%s/%n", START, siteInfo[0]);
+            } else {
+                //Print error message
+                System.out.printf("Unable to create directory%n");
+            }
+        }
         //Make html file
-        new File ();
+        File html = new File (START+siteInfo[0]+"/index.html");
         //Print creation verification
+        if(website.mkdirs()) {
+            if (html.isDirectory()) {
+                //Print confirmation message
+                System.out.printf("Created %s%s/%s%n", START, siteInfo[0], siteInfo[1]);
+            } else {
+                //Print error message
+                System.out.printf("Unable to create directory%n");
+            }
+        }
     }
 
     void writeToSite() {
         //Create new file object with relative path
-        File file = new File("");
+        File file = new File(START+siteInfo[0]+"/index.html");
         //Create buffered writer
-        BufferedWriter writer;
-        //Write website name and author to html file
-        writer.write();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
+            //Write website name and author to html file
+            writer.write("<!DOCTYPE html><html lang=\"en\">\n<head><title>"
+                    +siteInfo[0]+"</title></head>\n<meta>"+siteInfo[1]);
+            //Write template text
+            writer.write("\n<body><h1>\nThis is a template.\n</h1></body>\n</html>");
+            //Print confirmation
+            System.out.printf("Created %s%s/index.html%n" ,START, siteInfo[0]);
+        } catch (IOException e) {
+            //Print error message
+            System.out.printf("Unable to write to file%n");
+        }
     }
 
     void generateDirectories() {
         //If user chose to make java directory, create new directory
-        if(){
-            new File ();
+        if(folders[0]){
+            File java = new File (START+siteInfo[0]+"/js/");
             //Print creation verification
+            if (java.mkdirs()){
+                System.out.printf("Created %s%s/js/%n" ,START, siteInfo[0]);
+            }
         }
         //If user chose to make css directory, create new directory
-        if(){
-            new File ();
+        if(folders[1]){
+            File css = new File (START+siteInfo[0]+"/css/%n");
             //Print creation verification
+            if (css.mkdirs()) {
+                System.out.printf("Created %s%s/css/", START, siteInfo[0]);
+            }
         }
     }
 
@@ -76,9 +100,28 @@ class HTMLGenerator {
 
 public class Solution43 {
     public static void main(String[] args) {
-        //Call prompt info method
+        Scanner input = new Scanner(System.in);
+
+        //Prompt user for site name
+        System.out.printf("Please enter the site name:%n");
+        String name = input.nextLine();
+        System.out.printf("Please enter the site author:%n");
+        //Save user input
+        String author = input.nextLine();
+        System.out.printf("Do you want to generate a JavaScript directory? [Enter \"y\" or \"Y\" to generate]%n");
+        //Save user input
+        String java = input.nextLine();
+        System.out.printf("Do you want to generate a CSS directory? [Enter \"y\" or \"Y\" to generate]%n");
+        //Save user input
+        String css = input.nextLine();
+
+        //Call constructor
+        HTMLGenerator htmlG = new HTMLGenerator(name, author, java, css);
         //Call site directory generation
+        htmlG.generateSiteData();
         //Call html writing method
+        htmlG.writeToSite();
         //Call directory generation method
+        htmlG.generateDirectories();
     }
 }
---
+++ exercise43/src/test/java/baseline/HTMLGeneratorTest.java
@@ -0,0 +1,80 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HTMLGeneratorTest {
+
+    @Test
+    //Test if the base website folder is created
+    void testWebFolderCreation() {
+        //Setup directory/file creation
+        //BeforeEach Fails to carry over class instance objects
+        String name = "TestWebsite";
+        String author = "TestName";
+        String java = "Y";
+        String css = "n";
+        HTMLGenerator htmlG = new HTMLGenerator(name, author, java, css);
+
+        File testWeb = new File ("./data/website/TestWebsite");
+        htmlG.generateSiteData();
+        htmlG.writeToSite();
+        htmlG.generateDirectories();
+
+        assertTrue(testWeb.isDirectory());
+    }
+
+    @Test
+    //Test if the html file is created
+    void testHTMLFileCreation() {
+        String name = "TestWebsite";
+        String author = "TestName";
+        String java = "Y";
+        String css = "n";
+        HTMLGenerator htmlG = new HTMLGenerator(name, author, java, css);
+
+        File testFile = new File ("./data/website/TestWebsite/index.html");
+        htmlG.generateSiteData();
+        htmlG.writeToSite();
+        htmlG.generateDirectories();
+
+        assertTrue(testFile.isFile());
+    }
+
+    @Test
+    //Test if the js folder is created
+    void testJSFolderCreation() {
+        String name = "TestWebsite";
+        String author = "TestName";
+        String java = "Y";
+        String css = "n";
+        HTMLGenerator htmlG = new HTMLGenerator(name, author, java, css);
+
+        File testJS = new File ("./data/website/TestWebsite/js");
+        htmlG.generateSiteData();
+        htmlG.writeToSite();
+        htmlG.generateDirectories();
+
+        assertTrue(testJS.isDirectory());
+    }
+
+    @Test
+    //Test if the css file is not created
+    void testCSSFolderNotCreated() {
+        String name = "TestWebsite";
+        String author = "TestName";
+        String java = "Y";
+        String css = "n";
+        HTMLGenerator htmlG = new HTMLGenerator(name, author, java, css);
+
+        File testCSS = new File ("./data/website/TestWebsite/css");
+        htmlG.generateSiteData();
+        htmlG.writeToSite();
+        htmlG.generateDirectories();
+
+        assertFalse(testCSS.isDirectory());
+    }
+}
