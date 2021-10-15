d40be787a40969ccc5e4ec3a9ebbfc83296fe0c3
Rylan S <rylan.simpson.02@gmail.com>
2021-10-15 19:02:32 -0400

    Created skeleton, pseudocode, and UML diagram for exercise 46

---
+++ example46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,55 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Rylan Simpson
+ */
+
+package baseline;
+
+import java.io.File;
+import java.util.SortedMap;
+
+class WordFrequency {
+
+    SortedMap<String, Integer> wordCount;
+
+    void countWords(File fileName) {
+        //while the file has another string
+        while () {
+            //If the string is already a key in the map
+            if () {
+                //Increment the map value by 1
+            }
+            //else, add a map entry with the string as a key and the value starting at one
+            else {
+
+            }
+        }
+    }
+
+    void printResults() {
+        //For each entry int the map
+        for () {
+            //Print the key string
+            System.out.printf("%n");
+            //For the integer stored in the map
+            for () {
+                //Print a *
+                System.out.printf("%n");
+            }
+            //Buffer table
+            System.out.printf("%n");
+        }
+    }
+}
+
+public class Solution46 {
+    public static void main(String[] args) {
+        WordFrequency freq = new WordFrequency();
+        //Open file
+        File fileIn = new File();
+        //Call word counting method
+        freq.countWords();
+        //Call result printing method
+        freq.printResults();
+    }
+}
